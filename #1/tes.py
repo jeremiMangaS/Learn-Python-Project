984e149ab5fbcf6b36383b7465cfc2bc592df5d3
Mangatur Jeremi Silitonga <>
2026-01-11 20:43:08 +0700

    feat : Adding simple database using txt

--- #1/tes.py
+++ #1/tes.py
@@ -1,19 +1,42 @@
 # Still learning new things
 
 taskList = []
+fileName = "tugas.txt"
+
+def get_data() :
+    try :
+        f = open(fileName, "r")
+        if not f :
+            raise FileNotFoundError
+    except FileNotFoundError :  
+        print("Making file...")
+        f = open(fileName, "w")
+    else :    
+        for task in f.readlines() :
+            taskList.append(task.replace("\n", ""))
+        f.close()
 
 def add_task() :
     inputTask = input("Add new task : ")
+    write_data(inputTask)
     taskList.append(inputTask)
     print("[System] New task added")
 
+def write_data(inputTask) :
+    with open(fileName, "a") as f :
+        if len(taskList) == 0 :
+            f.write(f"{inputTask}")
+        else : 
+            f.write(f"\n{inputTask}")
+    f.close()
+
 def show_tasks() :
-    if len(taskList) < 1 :
-        print("List is empty.")
+    if len(taskList) == 0 :
+        print("There's no task yet ...")
     else :
         print("=============================")
         for i, task in enumerate(taskList) :
-            print(f"{i + 1}, {task}")
+            print(f"{i + 1}. {task}")
         print("=============================")
         
 def delete_task() :
@@ -21,14 +44,13 @@ def delete_task() :
         try :
             show_tasks()
             indx = int(input("Enter the task index : "))
-            if (indx - 1) <= len(taskList) and (indx - 1) > 0 : 
-                delete_process(indx)
+            delete_process(indx) 
         except IndexError : 
             print("Out of range")
         except :
-            print("")            
-        # except : print("Input must be number")
-        else : break
+            print("Input must be a number")
+        else : 
+            break
 
 def delete_process(indx) :
     while True :
@@ -36,14 +58,25 @@ def delete_process(indx) :
         usrinput = input("Confirm : [Y/n]")
         if usrinput == "Y" or usrinput == "y" :
             taskList.pop(indx - 1)
+            push_newdata()
             print(f"Delete successfull")
             return
-        elif usrinput == "N" or usrinput == "n" :
+        elif usrinput.lower() == "n" :
             print("Canceling...")
             return
-        else : print("the answer should be just y or n")        
+        else : print("the answer should be just y or n")  
+    
+def push_newdata() :
+    f = open(fileName, "w")
+    for i, task in enumerate(taskList) :
+        if i == 0 :
+            f.write(task)
+        else :
+            f.write(f"\n{task}")
+    
             
 def main() :
+    get_data()
     print("WELCOME TO-DO-LIST")
     while True :
         try :
@@ -52,10 +85,11 @@ def main() :
             inputMenu = int(input("Input : ")) 
         except :
             print("Input cannot be null and should be integer type!")
-        if inputMenu == 1 : add_task()
-        elif inputMenu == 2 : show_tasks() 
-        elif inputMenu == 3 : delete_task()
-        elif inputMenu == 0 : return False
-        else : print("Menu unregistered")
+        else :
+            if inputMenu == 1 : add_task()
+            elif inputMenu == 2 : show_tasks() 
+            elif inputMenu == 3 : delete_task()
+            elif inputMenu == 0 : return False
+            else : print("Menu unregistered")
 
 main()
